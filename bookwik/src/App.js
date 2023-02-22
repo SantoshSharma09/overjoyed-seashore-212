474bf3da9d6d41e0f92b3b96dd61710339843ca4
Gedelachandrasekhar126396 <gedelachandrasekhar@gmail.com>
2023-02-22 22:05:24 +0530

    Navbar&footer

--- bookwik/src/App.js
+++ bookwik/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer'
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">

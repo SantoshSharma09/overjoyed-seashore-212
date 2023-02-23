c075a38b2618257a7c30396b6da8088493ee5317
unknown <san.kr.sharma090@gmail.com>
2023-02-24 01:59:43 +0530

    updated

--- bookwik/src/routes/AllRoutes.jsx
+++ bookwik/src/routes/AllRoutes.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProductPage from "../ProductPage";
 import SingleProduct from "../SingleProduct";
-import HomePage from "../Pages/HomePage/HomePage"
+import HomePage from "../Pages/HomePage/HomePage";
 import SignUpPage from "../Pages/AccountsFolder/SignUp/SignUpPage";
 import LoginPage from "../Pages/AccountsFolder/Login/LoginPage";
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
         <Route path={"/products"} element={<ProductPage />} />
         <Route path={"/singleproduct/:id"} element={<SingleProduct />} />
-        <Route path="/signup" element={<SignUpPage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
